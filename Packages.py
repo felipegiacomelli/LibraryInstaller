2063c9b9abfb5f6c4fa54631c3aa07ef18c21dd0
Felipe Giacomelli <felipe.g@sinmec.ufsc.br>
2019-02-12 22:50:46 -0200

    Adding necessary packages

---
+++ Packages.py
@@ -0,0 +1,19 @@
+import os, subprocess
+
+packages = []
+packages.append("cmake")
+packages.append("bison")
+packages.append("flex")
+packages.append("diffutils")
+packages.append("wget")
+packages.append("dos2unix")
+packages.append("tcl")
+packages.append("tk")
+
+for package in packages:
+    print(package)
+
+# subprocess.call(["sh", "-c", "sudo pacman -Syu --noconfirm"])
+
+# for package in packages:
+    # subprocess.call(["sh", "-c", "sudo pacman -S --noconfirm --needed " + package])
