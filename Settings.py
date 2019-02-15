dcad2f012e72c99b4a7955d62c1a4b1791b4599f
Felipe Giacomelli <felipe.g@sinmec.ufsc.br>
2019-02-15 11:52:16 -0200

    Saving work

--- Settings.py
+++ Settings.py
@@ -1,29 +1,29 @@
 import os
 import multiprocessing
 
-systemPackages = True
+systemPackages = False
 
 compressedFiles = os.environ["LIBRARIES_FILES"]
 rootBuildDirectory = os.environ["HOME"] + "/Delete"
 rootInstallDirectory = os.environ["HOME"] + "/Libraries"
 
-buildType = "Release"
+buildType = "Debug"
 libraryType = "Shared"
 
-environmentVariables = True
+environmentVariables = False
 
 numberOfCores = str(multiprocessing.cpu_count())
 
 libraries = {
     "openmpi"  : {"version" : "3.0.1" , "install" : False},
-    "boost"    : {"version" : "1.68.0", "install" : False},
-    "petsc"    : {"version" : "3.10.2", "install" : False},
-    "cgns"     : {"version" : "3.3.1" , "install" : False},
-    "muparser" : {"version" : "2.2.6" , "install" : False},
+    "boost"    : {"version" : "1.68.0", "install" : True},
+    "petsc"    : {"version" : "3.10.2", "install" : True},
+    "cgns"     : {"version" : "3.3.1" , "install" : True},
+    "muparser" : {"version" : "2.2.6" , "install" : True},
     "hdf5"     : {"version" : "1.8.19", "install" : False},
     "metis"    : {"version" : "5.1.0" , "install" : False},
     "cgnstools": {"version" : "3.3.1" , "install" : False},
-    "mshtocgns": {"version" : "0.14.0", "install" : False},
+    "mshtocgns": {"version" : "0.14.0", "install" : True},
     "triangle" : {"version" : "1.6.0" , "install" : False},
     "tetgen"   : {"version" : "1.5.1" , "install" : False}
 }
