df0e987dad8d417e235554386b42aa39a0e7ea6d
Felipe Giacomelli <felipe.g@sinmec.ufsc.br>
2019-04-18 16:58:50 -0300

    Successfully updated libraries

--- Settings.py
+++ Settings.py
@@ -5,10 +5,7 @@ systemPackages = True
 
 rootBuildDirectory = os.environ["HOME"] + "/Delete"
 rootInstallDirectory = os.environ["HOME"] + "/Libraries"
-
 compressedFiles = rootInstallDirectory + "/targz"
-if "LIBRARIES_FILES" in os.environ:
-    compressedFiles = os.environ["LIBRARIES_FILES"]
 
 buildType = "Release"
 libraryType = "Shared"
@@ -21,10 +18,10 @@ libraries = {
     "openmpi"  : {"version" : "4.0.1" , "install" : True},
     "boost"    : {"version" : "1.70.0", "install" : True},
     "petsc"    : {"version" : "3.11.1", "install" : True},
-    "cgns"     : {"version" : "3.3.1" , "install" : False},
-    "muparser" : {"version" : "2.2.6" , "install" : False},
-    "mshtocgns": {"version" : "0.15.0", "install" : False},
-    "cgnstools": {"version" : "3.3.1" , "install" : False},
+    "cgns"     : {"version" : "3.3.1" , "install" : True},
+    "muparser" : {"version" : "2.2.6" , "install" : True},
+    "mshtocgns": {"version" : "0.15.0", "install" : True},
+    "cgnstools": {"version" : "3.3.1" , "install" : True},
     "triangle" : {"version" : "1.6.0" , "install" : False},
     "tetgen"   : {"version" : "1.5.1" , "install" : False},
     "hdf5"     : {"version" : "1.8.19", "install" : False},
