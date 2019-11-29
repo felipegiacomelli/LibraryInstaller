4463872c39bbf364fff84933dcf9edd8bb86d63d
Felipe Giacomelli <felipe.g@sinmec.ufsc.br>
2019-11-29 19:10:23 -0300

    Installing boost_python3.7

--- Libraries/Boost.py
+++ Libraries/Boost.py
@@ -13,8 +13,6 @@ class Boost(Library):
         self.flags["debug"]     = ""
         self.flags["release"]   = ""
 
-        self.pythonConfigureFlags = "--with-python=/usr/bin/python2.7 -with-python-root=/usr --with-python-version=2.7"
-
         self.downloadLink = "https://downloads.sourceforge.net/project/boost/boost/%s/boost_%s_%s_%s.tar.gz" % (self.version, self.versionMajor, self.versionMinor, self.versionPatch)
 
     def install(self):
@@ -29,8 +27,11 @@ class Boost(Library):
         Library.writeMessage(self, "Moving to source directory")
         os.chdir(self.sourceDirectory)
 
+        self.writeMessage("Copying user-config.jam")
+        Library.runCommand(self, "cp  %s/CMake/user-config.jam %s" % (self.rootDirectory, os.environ["HOME"]))
+
         self.writeMessage("Running configure")
-        self.runCommand("./bootstrap.sh %s --prefix=%s" % (self.pythonConfigureFlags, self.installDirectory))
+        self.runCommand("./bootstrap.sh --prefix=%s" % (self.installDirectory))
 
         self.writeMessage("Adding 'using mpi ;' to %s/project-config.jam" % self.sourceDirectory)
         projectConfig = open("%s/project-config.jam" % self.sourceDirectory, "a")
@@ -39,6 +40,9 @@ class Boost(Library):
 
         self.writeMessage("Building and installing")
         self.runCommand("./b2 variant=%s %s --prefix=%s -j %s install" % (self.buildType.lower(), self.flags["configure"], self.installDirectory, self.numberOfCores))
+
+        self.writeMessage("Removing user-config.jam")
+        Library.runCommand(self, "rm  %s/user-config.jam" % (os.environ["HOME"]))
 
         Library.displayEndMessage(self)
 
